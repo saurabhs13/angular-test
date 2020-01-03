7eb5364db4b14b33fe89d70a2987fc3750f21968
Root User <root@localhost>
2020-01-03 23:38:36 +0530

    module 3 complete solution

--- module3-solution/app.js
+++ module3-solution/app.js
@@ -3,33 +3,69 @@
 angular.module('NarrowItDownApp', [])
 .controller('NarrowItDownController', NarrowItDownController)
 .service('MenuSearchService', MenuSearchService)
-.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com");
+.constant('ApiBasePath', "https://davids-restaurant.herokuapp.com")
+.directive('foundItems',FoundItems);
+
+function FoundItems(){
+  var ddo = {
+    templateUrl:'founditems.html',
+    retrict:'E',
+    scope:{
+      list:'<items',
+      onRemove:'&'
+    },
+  };
+  return ddo;
+}
 
 NarrowItDownController.$inject = ['MenuSearchService'];
 
 function NarrowItDownController(MenuSearchService){
   var nidc = this;
-  nidc.found  = MenuSearchService.getMatchedMenuItems('Chicken');
+
+
+    nidc.getMatchedMenuItems = function(){
+      console.log('Search Term:',nidc.searchTerm);
+      if(typeof nidc.searchTerm == 'undefined' || nidc.searchTerm.trim() == ""){
+        nidc.categories = [];
+      }else{
+      var promise  = MenuSearchService.getMatchedMenuItems(nidc.searchTerm);
+
+      promise.then(function(response){
+      nidc.categories = response;
+      }).catch(function(error){
+
+      });
+      }
+    }
+
+    nidc.removeItem = function(itemIndex){
+      nidc.categories.splice(itemIndex,1);
+    }
+
 }
 MenuSearchService.$inject = ['$http', 'ApiBasePath'];
 function MenuSearchService($http, ApiBasePath) {
   var service = this;
 
   service.getMatchedMenuItems = function (searchTerm) {
-   return $http({
+   return  $http({
       method: "GET",
       url: (ApiBasePath + "/menu_items.json")
     }).then(function (response) {
-        var foundItems = response.data;
+        var reponseArray = response.data.menu_items;
+        var foundItems = [];
         console.log(foundItems);
-        console.log(foundItems.menu_items);
-        for (var i = 0; i < foundItems.menu_items.length; i++) {
-          if(foundItems.menu_items[i].description.indexOf(searchTerm) == -1){
-          foundItems.menu_items.splice(i,1);
+        console.log('Before Filter',reponseArray);
+        for (var i = 0; i < reponseArray.length; i++) {
+          if(reponseArray[i].description.indexOf(searchTerm) != -1){
+            foundItems.push(reponseArray[i]);
           }
         }
-      console.log(foundItems.menu_items);
-      return foundItems.menu_items;
+        console.log('After Filter',foundItems);
+        return foundItems;
+    },function(error){
+      console.log(error);
     });
 }
 
