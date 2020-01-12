c8b293983c5b84058a053be382e2a55131544c40
Root User <root@localhost>
2020-01-13 01:04:51 +0530

    module 5 task 1

--- module5-solution/src/public/signup/registration.controller.js
+++ module5-solution/src/public/signup/registration.controller.js
@@ -9,8 +9,9 @@ function RegistrationController(AccountService,MenuService) {
   var reg = this;
     reg.submit = function () {
       console.log("I m here",reg.user);
-      var response = MenuService.getMenuItemForShortName(reg.menunumber);
+      var response = MenuService.getMenuItemForShortName(reg.user.menunumber);
       response.then(function success(response) {
+        reg.menuItemerror = "";
         reg.completed = true;
         AccountService.registerUser(reg.user);
         console.log(response);
