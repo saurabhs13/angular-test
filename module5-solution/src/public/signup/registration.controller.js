0509c9abc50c8c9dcea3cd82e0e11bec40086608
Root User <root@localhost>
2020-01-15 02:09:32 +0530

    module 5 task 2 form validations

--- module5-solution/src/public/signup/registration.controller.js
+++ module5-solution/src/public/signup/registration.controller.js
@@ -8,13 +8,11 @@ RegistrationController.$inject = ['AccountService','MenuService'];
 function RegistrationController(AccountService,MenuService) {
   var reg = this;
     reg.submit = function () {
-
       var response = MenuService.getMenuItemForShortName(reg.user.menunumber);
       response.then(function success(response) {
         reg.menuItemerror = "";
         reg.completed = true;
         AccountService.registerUser(reg.user);
-        console.log(response);
       }, function error(response) {
         reg.menuItemerror="No such menu number exists";
       });
